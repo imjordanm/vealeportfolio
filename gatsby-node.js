72628bbfe2ead745320c507f4c250e45077b97e4
Jordan McRae <imjordanm@gmail.com>
2019-02-12 18:51:00 +1300

    Testing flickity webpack loader again.

--- gatsby-node.js
+++ gatsby-node.js
@@ -110,3 +110,18 @@ exports.createPages = ({ actions, graphql }) => {
     })
     }
 } */
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /flickity/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    });
+  }
+};
